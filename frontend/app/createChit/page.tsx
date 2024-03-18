85a51215663bffb6ddaf98caf42cc0f0c6d714fc
SamShah30112004 <harshdeepashah@gmail.com>
2024-03-19 01:30:51 +0530

    Added MUN Page and login to some pages

--- frontend/app/createChit/page.tsx
+++ frontend/app/createChit/page.tsx
@@ -1,46 +1,119 @@
-"use client";
-import React, {  useState } from "react";
+'use client';
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+import { UserAuth } from '../context/AuthContext';
+import axios from 'axios';
+import Link from 'next/link';
 
 const CreateChit = () => {
-    const [countries, setCountries] = useState([]);
-    return (
-        <div className="flex flex-row w-full text-white">
-            <div className="flex flex-col justify-center items-center w-3/4 m-5 border-gray-500 border-2 rounded-xl">
-                <h1 className="text-4xl font-bold m-5">Create Chit</h1>
-                <div className="p-10 flex flex-col w-full">
-                    <label className="mb-3 text-xl">Select Country</label>
-                    <select className="mb-3 max-w-full border rounded p-2 bg-black">
-                        {countries.map((country) => (
-                            <option key={country}>{country}</option>
-                        ))}
-                    </select>
-                    <label className="mb-3 text-xl">Select Query Type</label>
-                    <select className="mb-3 max-w-full border rounded p-2 bg-black">
-                        <option>India</option>
-                        <option>India</option>
-                        <option>India</option>
-                        <option>India</option>
-                    </select>
-                    <div className="mb-3">
-                        <input type="checkbox" id="viaEB" name="viaEB" value="Via EB" className="bg-black" />
-                        <label htmlFor="viaEB"> Via EB</label>
-                    </div>
-                    <label htmlFor="query" className="mb-3 text-xl">Chit Message:</label>
-                    <textarea className="mb-3 max-w-full text-lg border rounded p-2 bg-black"></textarea>
-                    <div className="flex flex-row my-4 space-x-2 justify-between">
-                        <button className="border border-red-500 text-red-500 rounded px-4 py-2">Reset</button>
-                        <button className="border border-green-500 text-green-500 rounded px-4 py-2">Send Message</button>
-                    </div>
-                </div>
-            </div>
-            <div className="flex flex-col justify-center items-center w-1/2 m-5 border-gray-500 border-2 rounded-xl">
-                <div className="flex flex-row space-x-2 justify-between">
-                    <button className="border border-red-500 text-red-500 rounded px-4 py-2">Upload Document</button>
-                    <button className="border border-green-500 text-green-500 rounded px-4 py-2">Upload Image</button>
-                </div>
-            </div>
+  const [countries, setCountries] = useState([]);
+  const { user, googleSignIn, logOut } = UserAuth();
+  const handleSignIn = async () => {
+    try {
+      await googleSignIn();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSignOut = async () => {
+    try {
+      await logOut();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .post(
+          'http://127.0.0.1:8001/email_sign_up',
+          JSON.stringify({
+            email: user.email,
+          }),
+          {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          }
+        )
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [user]);
+  return (
+    <>
+      <nav className="flex items-center justify-between bg-black p-6">
+        <div className="flex items-center">
+          <Image src="/logo.png" alt="Company Logo" width={30} height={30} className="mr-2" />
+          <span className="text-white text-lg">SummitSync</span>
+          <Link href="/createChit" className="text-white mr-4 ml-8 active:text-blue-500">
+            Create Chit
+          </Link>
+          <Link href="/messages" className="text-white mr-4">
+            View Chit
+          </Link>
+          <Link href="/joinRegister" className="text-white mr-4">
+            Join Comittee
+          </Link>
         </div>
-    );
+        <div className="flex items-center">
+          {!user ? (
+            <button onClick={handleSignIn} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Sign In
+            </button>
+          ) : (
+            <button onClick={handleSignOut} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+              Sign Out
+            </button>
+          )}
+        </div>
+      </nav>
+      <div className="flex flex-row w-full text-white">
+        <div className="flex flex-col justify-center items-center w-3/4 m-5 border-gray-500 border-2 rounded-xl">
+          <h1 className="text-4xl font-bold m-5">Create Chit</h1>
+          <div className="p-10 flex flex-col w-full">
+            <label className="mb-3 text-xl">Select Country</label>
+            <select className="mb-3 max-w-full border rounded p-2 bg-black">
+              {countries.map((country) => (
+                <option key={country}>{country}</option>
+              ))}
+            </select>
+            <label className="mb-3 text-xl">Select Query Type</label>
+            <select className="mb-3 max-w-full border rounded p-2 bg-black">
+              <option>India</option>
+              <option>India</option>
+              <option>India</option>
+              <option>India</option>
+            </select>
+            <div className="mb-3">
+              <input type="checkbox" id="viaEB" name="viaEB" value="Via EB" className="bg-black" />
+              <label htmlFor="viaEB"> Via EB</label>
+            </div>
+            <label htmlFor="query" className="mb-3 text-xl">
+              Chit Message:
+            </label>
+            <textarea className="mb-3 max-w-full text-lg border rounded p-2 bg-black"></textarea>
+            <div className="flex flex-row my-4 space-x-2 justify-between">
+              <button className="border border-red-500 text-red-500 rounded px-4 py-2">Reset</button>
+              <button className="border border-green-500 text-green-500 rounded px-4 py-2">Send Message</button>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col justify-center items-center w-1/2 m-5 border-gray-500 border-2 rounded-xl">
+          <div className="flex flex-row space-x-2 justify-between">
+            <button className="border border-red-500 text-red-500 rounded px-4 py-2">Upload Document</button>
+            <button className="border border-green-500 text-green-500 rounded px-4 py-2">Upload Image</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default CreateChit;
--- frontend/app/messages/page.tsx
+++ frontend/app/messages/page.tsx
@@ -1,7 +1,79 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+import { UserAuth } from '../context/AuthContext';
+import axios from 'axios';
+import Link from 'next/link';
 
 const Messages = () => {
-    return (
+  const [countries, setCountries] = useState([]);
+  const { user, googleSignIn, logOut } = UserAuth();
+  const handleSignIn = async () => {
+    try {
+      await googleSignIn();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSignOut = async () => {
+    try {
+      await logOut();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .post(
+          'http://127.0.0.1:8001/email_sign_up',
+          JSON.stringify({
+            email: user.email,
+          }),
+          {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          }
+        )
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [user]);
+  return (
+    <>
+      <nav className="flex items-center justify-between bg-black p-6">
+        <div className="flex items-center">
+          <Image src="/logo.png" alt="Company Logo" width={30} height={30} className="mr-2" />
+          <span className="text-white text-lg">SummitSync</span>
+          <Link href="/createChit" className="text-white mr-4 ml-8 active:text-blue-500">
+            Create Chit
+          </Link>
+          <Link href="/messages" className="text-white mr-4">
+            View Chit
+          </Link>
+          <Link href="/joinRegister" className="text-white mr-4">
+            Join Comittee
+          </Link>
+        </div>
+        <div className="flex items-center">
+          {!user ? (
+            <button onClick={handleSignIn} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Sign In
+            </button>
+          ) : (
+            <button onClick={handleSignOut} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+              Sign Out
+            </button>
+          )}
+        </div>
+      </nav>
         <div className="flex flex-row w-full text-white">
             <div className="flex flex-col justify-center items-center w-3/4 m-5">
                 <div className="flex flex-col w-full m-3 border-gray-500 border-2 rounded-xl">
@@ -48,6 +120,7 @@ const Messages = () => {
                 </div>
             </div>
         </div>
+        </>
     );
 };
 
