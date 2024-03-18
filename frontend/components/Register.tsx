fb24334a161083b411b55cc0ef0aa4b0a7bf3a1f
SamShah30112004 <harshdeepashah@gmail.com>
2024-03-18 16:21:44 +0530

    Added frontend pages

--- frontend/components/Register.tsx
+++ frontend/components/Register.tsx
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { UserAuth } from '../app/context/AuthContext';
+import { TypewriterEffectSmooth } from './ui/typewriter-effect';
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
 
 const Register = () => {
   const { user, googleSignIn, logOut } = UserAuth();
   const [loading, setLoading] = useState(true);
-
+  const router = useRouter();
   const handleSignIn = async () => {
     try {
       await googleSignIn();
+      router.push('/joinRegister');
     } catch (error) {
       console.log(error);
     }
@@ -31,29 +35,59 @@ const Register = () => {
 
   useEffect(() => {
     if (user) {
-      axios.post('127.0.0.1:8001/email_sign_up', {
-        email: user.email
-      })
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+      axios
+        .post(
+          'http://127.0.0.1:8001/email_sign_up',
+          JSON.stringify({
+            email: user.email,
+          }),
+          {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          }
+        )
+        .then((response) => {
+          console.log(response);
+        });
     }
   }, [user]);
 
+  const words = [
+    {
+      text: 'Login Now',
+      className: 'text-3xl',
+    },
+    {
+      text: '& Register',
+      className: 'text-3xl',
+    },
+    {
+      text: 'to Join',
+      className: 'text-3xl',
+    },
+    {
+      text: 'SummitSync',
+      className: 'text-blue-500 dark:text-blue-500 text-3xl',
+    },
+  ];
+
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-screen ">
       {loading ? null : !user ? (
-        <button onClick={handleSignIn} className="p-2 cursor-pointer bg-blue-500 text-white rounded">
-          Sign in with Google
-        </button>
+        <div className="border border-blue-500 rounded-lg p-12 flex flex-col items-center justify-center w-[45rem]">
+          <div className="w-98 ml-6 text-center text-white">
+            <TypewriterEffectSmooth words={words} />
+          </div>
+          <button onClick={handleSignIn} className="p-2 cursor-pointer bg-blue-500 text-white rounded">
+            Sign in with Google
+          </button>
+        </div>
       ) : (
         <div>
           <p className="text-white">Welcome, {user.email}</p>
           <button onClick={handleSignOut} className="p-2 cursor-pointer bg-blue-500 text-white rounded">
-           Sign Out
+            Sign Out
           </button>
         </div>
       )}
--- frontend/components/ui/lamp.tsx
+++ frontend/components/ui/lamp.tsx
@@ -20,7 +20,7 @@ export const LampContainer = ({ children, className }: { children: React.ReactNo
           }}
           className="absolute inset-auto right-1/2 h-56 overflow-visible w-[30rem] bg-gradient-conic from-cyan-500 via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top]">
           <div className="absolute w-[100%] left-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
-          <div className="absolute w-40 h-[100%] left-0 bg-slate-950  bottom-0 z-20 [mask-image:linear-gradient(to_right,white,transparent)]" />
+          <div className="absolute mt-12 w-40 h-[100%] left-0 bg-slate-950  bottom-0 z-20 [mask-image:linear-gradient(to_right,white,transparent)]" />
         </motion.div>
         <motion.div
           initial={{ opacity: 0.5, width: '15rem' }}
@@ -34,8 +34,8 @@ export const LampContainer = ({ children, className }: { children: React.ReactNo
             backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
           }}
           className="absolute inset-auto left-1/2 h-56 w-[30rem] bg-gradient-conic from-transparent via-transparent to-cyan-500 text-white [--conic-position:from_290deg_at_center_top]">
-          <div className="absolute  w-40 h-[100%] right-0 bg-slate-950  bottom-0 z-20 [mask-image:linear-gradient(to_left,white,transparent)]" />
-          <div className="absolute  w-[100%] right-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
+          <div className="absolute w-40 h-[100%] right-0 bg-slate-950  bottom-0 z-20 [mask-image:linear-gradient(to_left,white,transparent)]" />
+          <div className="absolute w-[100%] right-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
         </motion.div>
         <div className="absolute top-1/2 h-48 w-full translate-y-12 scale-x-150 bg-slate-950 blur-2xl"></div>
         <div className="absolute top-1/2 z-50 h-48 w-full bg-transparent opacity-10 backdrop-blur-md"></div>
@@ -62,7 +62,7 @@ export const LampContainer = ({ children, className }: { children: React.ReactNo
         <div className="absolute inset-auto z-40 h-44 w-full -translate-y-[12.5rem] bg-slate-950 "></div>
       </div>
 
-      <div className="relative z-50 flex -translate-y-80 flex-col items-center justify-center px-5">{children}</div>
+      <div className="relative z-50 flex -translate-y-80 flex-col items-center justify-center px-5 ">{children}</div>
     </div>
   );
 };
